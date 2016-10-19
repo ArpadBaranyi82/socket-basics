d4ad8da5550ba323487fc9ffc60d5ed1f62514ac
Arpad Baranyi <azb4782prgng@gmail.com>
2016-10-19 11:27:51 +0200

    timestamp added

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,10 @@ socket.on("connect", function(){
 });
 
 socket.on("message", function(message){
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log("new message: " + message.text);
 	
-	$(".messages").append("<p>" + message.text + "</p>");
+	$(".messages").append("<p><strong>" + momentTimestamp.local().format("h:mm a") + ": </strong>" +  message.text + "</p>");
 });
 
 
