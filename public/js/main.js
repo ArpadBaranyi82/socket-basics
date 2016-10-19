4f64f4cd23b12ee6b9f7f26e24be55806502333d
Arpad Baranyi <azb4782prgng@gmail.com>
2016-10-19 08:34:53 +0200

    form added

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,19 @@ socket.on("connect", function(){
 
 socket.on("message", function(message){
 	console.log("new message: " + message.text);
+});
+
+$(document).ready(function(){
+	var $form = $("#message-form");
+	$form.on("submit", function(event){
+		event.preventDefault();
+		
+		var $message = $form.find("input[name=message]");
+		
+		socket.emit("message", {
+			text: $message.val()
+		});
+		
+		$message.val("");
+	});
 })
