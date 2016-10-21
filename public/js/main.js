65ec08a293541af7868fdd921b1f1f5e9c5f16e7
Arpad Baranyi <azb4782prgng@gmail.com>
2016-10-21 07:54:32 +0200

    update

--- public/js/main.js
+++ public/js/main.js
@@ -19,11 +19,14 @@ $(document).ready(function(){
 	
 socket.on("message", function(message){
 	var momentTimestamp = moment.utc(message.timestamp);
-	var $message = $(".messages");
+	var $messages = $(".messages");
+	var $message = $("<li class='list-group-item'></li>");
+	
 	console.log("new message: " + message.text);
 	
 	$message.append("<p><strong>" + message.name + " " + momentTimestamp.local().format("h:mm a") + "</strong></p>");
 	$message.append("<p>" + message.text + "</p>");
+	$messages.append($message);
 	});
 
 
